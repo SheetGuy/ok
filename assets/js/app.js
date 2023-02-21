576719a784f72b83c757d0d39a1bf92f0b6ca77f
Noname <120730486+SheetGuy@users.noreply.github.com>
2023-02-21 08:52:42 -0300

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var swthBorder = true;
+var swthBg = true;
+var swthWaterMark = true;
+
+function switchToggle(switchClass) {
+   var element = document.getElementById(switchClass);
+   element.classList.toggle("on");
+}
